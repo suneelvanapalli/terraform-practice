d3f7de4c02c924f6a825f8e3ac0d44c0b3030859
SuneelKumar Vanapalli <suneel.vanapalli@adudev.com>
2023-04-16 13:11:03 +1000

    init changes

---
+++ functionapp.tf
@@ -0,0 +1,29 @@
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "tfteststgacct"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "example" {
+  name                = "tfdemofuncapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
